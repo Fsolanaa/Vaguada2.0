7022d7ee17967509ab15662f0254dc24cc2a4ab2
Juan <jestebanl88m@gmail.com>
2024-10-10 15:38:53 +0200

    form registro arreglado

--- Centros_Unico/include/formRegistro.php
+++ Centros_Unico/include/formRegistro.php
@@ -5,34 +5,34 @@
             <img src="img/cerrar.png" alt="cerrar" />
         </div>
         <div class="user-box">
-            <input  name="nombre" id="nombre" type="text">
+            <input  name="nombre" id="nombre" type="text" pattern="^[A-Z][a-z]{1,25}" required title="Por favor introduzca un nombre válido">
             <label>Nombre completo</label>
-            <p id="corregirNombre" class="mensajeError"></p>
+            <!-- <p id="corregirNombre" class="mensajeError"></p> -->
         </div>
         <div class="user-box">
-            <input  name="telefono" id="telefono" type="text">
+            <input  name="telefono" id="telefono" type="text" pattern="[0-9]{9,}" required title="El número de teléfono debe tener al menos 9 números">
             <label>Telefono</label>
-            <p id="corregirTelefono" class="mensajeError"></p>
+            <!-- <p id="corregirTelefono" class="mensajeError"></p> -->
         </div>
         <div class="user-box date">
-            <input  name="nacimiento" id="nacimiento" type="date">
+            <input  name="nacimiento" id="nacimiento" type="date" required>
             <label id="campoFecha">Fecha de nacimiento</label>
-            <p id="corregirFecha" class="mensajeError"></p>
+            <!-- <p id="corregirFecha" class="mensajeError"></p> -->
         </div>
         <div class="user-box">
-            <input name="email" id="email" type="text">
+            <input name="email" id="email" type="email" size="30" pattern="[a-zA-Z0-9._%+-]+@[a-z]\.com" required title="Por favor introduzca un correo válido">
             <label>Correo electrónico</label>
-            <p id="corregirEmail" class="mensajeError"></p>
+            <!-- <p id="corregirEmail" class="mensajeError"></p> -->
         </div>
         <div class="user-box">
-            <input id="password" name="password" type="password">
+            <input id="password" name="password" type="password" required pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@_$!%*?&])[A-Za-z\d@_$!%*?&]{5,}" title="La contraseña debe tener al menos 5 caracteres, minúsculas, mayúsculas, un número y un carácter especial">
             <label>Contraseña</label>
-            <p id="corregirContrasena" class="mensajeError"></p>
+            <!-- <p id="corregirContrasena" class="mensajeError"></p> -->
             <!-- Añadimos dos div que nos mostrarán la seguridad de la contraseña en tiempo real -->
             <div id="password-strength-meter"></div>
             <div id="password-strength-text"></div>
         </div>
-        <button href="#" id="btnCrearCuenta" type="submit">Crear cuenta</button>
+        <button id="btnCrearCuenta" type="submit">Crear cuenta</button>
     </form>
     <p>¿Ya tienes una cuenta? <a href="#" class="a2" id="btnInicio">Inicia sesión</a></p>
 
--- Centros_Unico/js/validacionRegistro.js
+++ Centros_Unico/js/validacionRegistro.js
@@ -1,16 +1,16 @@
 let form = document.getElementById('formularioRegistro');
 
-let nombre = document.getElementById('nombre');
-let corregirNombre = document.getElementById('corregirNombre');
+// let nombre = document.getElementById('nombre');
+// let corregirNombre = document.getElementById('corregirNombre');
 
-let telefono = document.getElementById('telefono');
-let corregirTelefono = document.getElementById('corregirTelefono');
+// let telefono = document.getElementById('telefono');
+// let corregirTelefono = document.getElementById('corregirTelefono');
 
-let nacimiento = document.getElementById('nacimiento');
-let corregirFecha = document.getElementById('corregirFecha');
+// let nacimiento = document.getElementById('nacimiento');
+// let corregirFecha = document.getElementById('corregirFecha');
 
-let email = document.getElementById('email');
-let corregirEmail = document.getElementById('corregirEmail');
+// let email = document.getElementById('email');
+// let corregirEmail = document.getElementById('corregirEmail');
 
 let password = document.getElementById('password');
 let corregirContrasena = document.getElementById('corregirContrasena');
@@ -23,78 +23,81 @@ function validarFormulario(evento) {
     evento.preventDefault();
     let validar = true;  // Esta variable debe resetearse en cada envío del formulario
 
-    validar = validarNombre();
-    validar = validarTelefono();
-    validar = validarFecha();
-    validar = validarEmail();
+    // validar = validarNombre();
+    // validar = validarTelefono();
+    // validar = validarFecha();
+    // validar = validarEmail();
     validar = validarContrasena();
 
     if (validar === true) {
         form.submit();  // Solo se enviará si todas las validaciones son correctas
+        document.getElementById("formRegistro").style.display = "none";
+        document.getElementById("cuerpo").style.filter = "none";
+        document.getElementById("header").style.filter = "none";
+        document.getElementById("nav").style.filter = "none";
+        document.getElementById("cajaContacto").style.filter = "none";
+        
     }
 }
 
-function validarNombre() {
-    if (nombre.value.trim() === "" || !/^[a-zA-Z0-9\s@$!%*?&]{1,25}$/.test(nombre.value)) {
-        nombre.style.border = "1px solid red";
-        corregirNombre.style.color = 'red';
-        corregirNombre.innerHTML = "El nombre debe tener entre 1 y 25 caracteres sin caracteres especiales";
-        return false;
-    } else {
-        nombre.style.border = "1px solid green";
-        corregirNombre.innerHTML = "";
-        return true;
-    }
-}
+// function validarNombre() {
+//     if (nombre.value.trim() === "" || !/^[a-zA-Z0-9\s@$!%*?&]{1,25}$/.test(nombre.value)) {
+//         nombre.style.border = "1px solid red";
+//         corregirNombre.style.color = 'red';
+//         corregirNombre.innerHTML = "El nombre debe tener entre 1 y 25 caracteres sin caracteres especiales";
+//         return false;
+//     } else {
+//         nombre.style.border = "1px solid green";
+//         corregirNombre.innerHTML = "";
+//         return true;
+//     }
+// }
 
-function validarTelefono() {
-    if (telefono.value.trim() === "" || !/^[0-9]{1,25}$/.test(telefono.value)) {
-        telefono.style.border = "1px solid red";
-        corregirTelefono.style.color = 'red';
-        corregirTelefono.innerHTML = "El teléfono solo debe tener números y hasta 25 dígitos";
-        return false;
-    } else {
-        telefono.style.border = "1px solid green";
-        corregirTelefono.innerHTML = "";
-        return true;
-    }
-}
+// function validarTelefono() {
+//     if (telefono.value.trim() === "" || !/^[0-9]{1,25}$/.test(telefono.value)) {
+//         telefono.style.border = "1px solid red";
+//         corregirTelefono.style.color = 'red';
+//         corregirTelefono.innerHTML = "El teléfono solo debe tener números y hasta 25 dígitos";
+//         return false;
+//     } else {
+//         telefono.style.border = "1px solid green";
+//         corregirTelefono.innerHTML = "";
+//         return true;
+//     }
+// }
 
-function validarFecha() {
-    if (nacimiento.value.trim() === "") {
-        nacimiento.style.border = "1px solid red";
-        corregirFecha.style.color = 'red';
-        corregirFecha.innerHTML = "Debes poner una fecha";
-        return false;
-    } else {
-        nacimiento.style.border = "1px solid green";
-        corregirFecha.innerHTML = "";
-        return true;
-    }
-}
+// function validarFecha() {
+//     if (nacimiento.value.trim() === "") {
+//         nacimiento.style.border = "1px solid red";
+//         corregirFecha.style.color = 'red';
+//         corregirFecha.innerHTML = "Debes poner una fecha";
+//         return false;
+//     } else {
+//         nacimiento.style.border = "1px solid green";
+//         corregirFecha.innerHTML = "";
+//         return true;
+//     }
+// }
 
-function validarEmail() {
-    if (email.value.trim() === "" || !/^[a-zA-Z0-9\s\.]{1,40}@[a-zA-Z0-9\s\.]{1,25}$/.test(email.value)) {
-        email.style.border = "1px solid red";
-        corregirEmail.style.color = 'red';
-        corregirEmail.innerHTML = "El email debe ser válido (contener @)";
-        return false;
-    } else {
-        email.style.border = "1px solid green";
-        corregirEmail.innerHTML = "";
-        return true;
-    }
-}
+// function validarEmail() {
+//     if (email.value.trim() === "" || !/^[a-zA-Z0-9\s\.]{1,40}@[a-zA-Z0-9\s\.]{1,25}$/.test(email.value)) {
+//         email.style.border = "1px solid red";
+//         corregirEmail.style.color = 'red';
+//         corregirEmail.innerHTML = "El email debe ser válido (contener @)";
+//         return false;
+//     } else {
+//         email.style.border = "1px solid green";
+//         corregirEmail.innerHTML = "";
+//         return true;
+//     }
+// }
 
 function validarContrasena() {
-    if (password.value.trim() === "" || !/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{5,}$/.test(password.value)) {
-        password.style.border = "1px solid red";
-        corregirContrasena.style.color = 'red';
-        corregirContrasena.innerHTML = "La contraseña debe tener al menos 5 caracteres, minúsculas, mayúsculas, un número y un carácter especial";
-        return false;
-    } else {
-        password.style.border = "1px solid green";
-        corregirContrasena.innerHTML = "";
+    if (password.value.trim() === "" || /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{5,}$/.test(password.value)) {
+        
         return true;
+    } else {
+        
+        return false;
     }
 }
